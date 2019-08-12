d6de9fc2d441610499eab254f4a950eed3ffbdc5
Dimitris Karakasilis <DKarakasilis@suse.com>
2019-08-12 08:49:50 +0000

    Add Kube auth builder

--- authenticators/kube_auth.go
+++ authenticators/kube_auth.go
@@ -1,13 +1,7 @@
 package authenticators
 
 import (
-	"encoding/json"
-	"fmt"
-
-	"code.cloudfoundry.org/bbs"
-	"code.cloudfoundry.org/bbs/models"
-	"code.cloudfoundry.org/diego-ssh/proxy"
-	"code.cloudfoundry.org/diego-ssh/routes"
+	cfauth "code.cloudfoundry.org/diego-ssh/authenticators"
 	"code.cloudfoundry.org/lager"
 	"golang.org/x/crypto/ssh"
 )
@@ -15,11 +9,10 @@ import (
 type kubeBuilder struct {
 }
 
-func NewKubeAuth() PermissionsBuilder {
+func NewKubeAuth() cfauth.PermissionsBuilder {
 	return &kubeBuilder{}
 }
 
-func (pb *permissionsBuilder) Build(logger lager.Logger, processGuid string, index int, metadata ssh.ConnMetadata) (*ssh.Permissions, error) {
-	actual, err := pb.bbsClient.ActualLRPGroupByProcessGuidAndIndex(logger, processGuid, index)
-	return nil, err
+func (kb *kubeBuilder) Build(logger lager.Logger, processGuid string, index int, metadata ssh.ConnMetadata) (*ssh.Permissions, error) {
+	return nil, nil
 }
