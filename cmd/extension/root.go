dda52adeb0f995b04b1c5d361b5562002bfbf64a
Ettore Di Giacinto <edigiacinto@suse.com>
2019-12-16 15:48:28 +0100

    Use Viper and cobra for cmd
    Split start and register in subcommands as we do in eirini-persi
    
    Signed-off-by: Dimitris Karakasilis <dkarakasilis@suse.com>

---
+++ cmd/extension/root.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	golog "log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc" // from https://github.com/kubernetes/client-go/issues/345
+)
+
+var (
+	log *zap.SugaredLogger
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "eirini-ext",
+	Short: "eirini-ext-operator manages Eirini apps on Kubernetes",
+}
+
+// Execute the root command, runs the server
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		golog.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	pf := rootCmd.PersistentFlags()
+
+	pf.StringP("kubeconfig", "c", "", "Path to a kubeconfig, not required in-cluster")
+	pf.StringP("namespace", "n", "eirini", "Namespace to watch for Eirini apps")
+	pf.StringP("operator-webhook-host", "w", "", "Hostname/IP under which the webhook server can be reached from the cluster")
+	pf.StringP("operator-webhook-port", "p", "2999", "Port the webhook server listens on")
+	pf.StringP("operator-service-name", "s", "", "Service name where the webhook runs on (Optional, only needed inside kube)")
+	pf.StringP("operator-webhook-namespace", "t", "", "The namespace the services lives in (Optional, only needed inside kube)")
+}
+
+// initConfig is executed before running commands
+func initConfig() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		golog.Fatalf("cannot initialize ZAP logger: %v", err)
+	}
+	log = logger.Sugar()
+}
---
+++ cmd/extension/version.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/SUSE/eirini-ssh/version"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Eirini Extensions Operator Version: %s\n", version.Version)
+		fmt.Printf("Go Version: %s\n", runtime.Version())
+		fmt.Printf("Go OS/Arch: %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	},
+}
